b3e7bc357dbe2e6d0f91a5ac613891c97ff4eed3
Aditya Gupta <adityagupta2087@gmail.com>
2026-03-09 14:25:16 +0530

    color palette generation

--- src/commands/select-image.ts
+++ src/commands/select-image.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import { tryCatch } from '../utils/result';
+// import Colorthief from "colorthief";
 
 const imagePickerOptions: vscode.OpenDialogOptions = {
     canSelectFiles: true,
@@ -12,21 +13,52 @@ const imagePickerOptions: vscode.OpenDialogOptions = {
 };
 
 export async function selectImageCommandCallback() {
-    const { data, err } = await tryCatch(chooseImageAndApplyTheme(imagePickerOptions)); 
+    const { data, err } = await tryCatch(pickImageTry(imagePickerOptions));
 
     if (err) {
-        vscode.window.showErrorMessage(err.message); 
-        return; 
+        vscode.window.showErrorMessage(err.message);
+        return;
     }
+
+    await generateThemeTry(data); 
 }
 
-async function chooseImageAndApplyTheme(options: vscode.OpenDialogOptions): Promise<string> {
+async function pickImageTry(options: vscode.OpenDialogOptions): Promise<string> {
     vscode.window.showInformationMessage('pick your image');
-    const selectedFiles: vscode.Uri[] | undefined = await vscode.window.showOpenDialog(imagePickerOptions);
+    const selectedFiles: vscode.Uri[] | undefined = await vscode.window.showOpenDialog(options);
 
     if (!selectedFiles) {
         throw new Error("select at least one image");
     }
-    
+
     return selectedFiles[0].fsPath;
+}
+
+async function generateThemeTry(imagePath: string) {
+    // const palette = await Colorthief.getPalette(imagePath);
+
+    // if (!palette) {
+    //     throw new Error("failed to get color palette");
+    // }
+
+    const colors = {
+        "editor.background": "#000000",
+        "activityBar.background": "#000000",
+        "sideBar.background": "#000000",
+        "statusBar.background": "#000000",
+        "button.background": "#000000"
+    };
+
+    const config = vscode.workspace.getConfiguration();
+
+    await config.update(
+        "workbench.colorCustomizations",
+        colors,
+        vscode.ConfigurationTarget.Global
+    );
+
+    vscode.window.showInformationMessage(
+        "Theme generated from image and applied!"
+    );
+
 }
