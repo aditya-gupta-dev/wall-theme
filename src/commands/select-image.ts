195242ebb7b88df79d7c6982dd70a8f394311298
Aditya Gupta <adityagupta2087@gmail.com>
2026-03-14 00:15:59 +0530

    registered theme

--- src/commands/select-image.ts
+++ src/commands/select-image.ts
@@ -1,6 +1,11 @@
 import * as vscode from 'vscode';
 import { tryCatch } from '../utils/result';
-import { Vibrant } from "node-vibrant/node"; 
+import { Vibrant } from "node-vibrant/node";
+import fs from "fs/promises";
+import path from 'path';
+import os from "os";
+import { Palette } from '../models/color';
+import { isFileExists } from '../utils/fs';
 
 const imagePickerOptions: vscode.OpenDialogOptions = {
     canSelectFiles: true,
@@ -12,15 +17,31 @@ const imagePickerOptions: vscode.OpenDialogOptions = {
     title: "Choose Image",
 };
 
-export async function selectImageCommandCallback() {
-    const { data, err } = await tryCatch(pickImageTry(imagePickerOptions));
+const themeFilePath = "./themes/generated-theme.json";
 
-    if (err) {
-        vscode.window.showErrorMessage(err.message);
+export async function selectImageCommandCallback() {
+    const imagePickResult = await tryCatch(pickImageTry(imagePickerOptions));
+
+    if (imagePickResult.err) {
+        vscode.window.showErrorMessage(imagePickResult.err.message);
         return;
     }
 
-    await generateThemeTry(data); 
+    const generateThemeResult = await tryCatch(generateThemeTry(imagePickResult.data));
+
+    if (generateThemeResult.err) {
+        vscode.window.showErrorMessage(generateThemeResult.err.message);
+        return;
+    }
+
+    const updateThemeFileResult = await tryCatch(updateThemeFileTry(generateThemeResult.data));
+
+    if (updateThemeFileResult.err) {
+        vscode.window.showErrorMessage(updateThemeFileResult.err.message);
+        return;
+    }
+
+    await updateEditorTheme();
 }
 
 async function pickImageTry(options: vscode.OpenDialogOptions): Promise<string> {
@@ -34,34 +55,50 @@ async function pickImageTry(options: vscode.OpenDialogOptions): Promise<string> 
     return selectedFiles[0].fsPath;
 }
 
-async function generateThemeTry(imagePath: string) {
+async function generateThemeTry(imagePath: string): Promise<Palette> {
 
-    const { data, err } = await tryCatch(Vibrant.from(imagePath).getPalette()); 
+    const { data, err } = await tryCatch(Vibrant.from(imagePath).getPalette());
 
-    if(err) { 
-        throw err; 
+    if (err) {
+        throw err;
     }
-    
-    console.log(data); 
 
-    const colors = {
-        "editor.background": data.DarkMuted?.hex,
-        "activityBar.background": data.DarkVibrant?.hex,
-        "sideBar.background": data.LightMuted?.hex,
-        "statusBar.background": data.LightVibrant?.hex,
-        "button.background": data.Muted?.hex
+    return {
+        light: JSON.stringify(data.DarkMuted?.toJSON())
     };
+}
 
+// TODO: complete the palette type 
+async function updateThemeFileTry(palette: Palette) {
+
+    const filePath = path.join(os.homedir(), ".config", "Code", themeFilePath);
+
+    if (!(await isFileExists(filePath))) { 
+        await fs.open(filePath, 'w'); 
+    }
+
+        const { err } = await tryCatch(
+            fs.writeFile(
+                filePath,
+                JSON.stringify({ ad: Math.random().toString() }, null, 2),
+            )
+        );
+
+    if (err) {
+        throw err;
+    }
+}
+
+async function updateEditorTheme() {
     const config = vscode.workspace.getConfiguration();
 
     await config.update(
         "workbench.colorCustomizations",
-        colors,
-        vscode.ConfigurationTarget.Global
+        "Image Theme",
+        true
     );
 
     vscode.window.showInformationMessage(
         "Theme generated from image and applied!"
     );
-
 }
